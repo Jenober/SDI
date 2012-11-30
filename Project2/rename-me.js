a2ed135348a1434dfaa9133eb4b8cee34a6f37d1
Houston Bennett <Jenober@fullsail.edu>
2012-11-29 17:09:22 -0800

    Started work. Beginning with "Procedure" on flowchart

---
+++ Project2/rename-me.js
@@ -0,0 +1,43 @@
+//alert("JavaScript works!");
+var playerName = "Grok"
+
+
+// Procedure
+var getReady = function (equipped, rested) {
+
+    if (equipped = true && rested = true) {
+        Console.log("You are rested and equipped for combat " + playerName + ".");
+
+
+    }
+    else {
+        console.log("You are too tired and ill-equipped to fight right now, "+ playerName + ".")
+    }
+
+
+};
+
+
+// Number function (While-loop)
+
+// battle code. While Mob's HP > 0 kill kill kill
+// Then goto boolean function for next battle or end.
+
+
+// Boolean function (Comparison)
+
+// Is Grok dead? Are the monsters all dead?
+// If Grok is dead. Sad ending. Call String function.
+// if Grok !dead next monster.
+// If mob's all dead. Happy ending. Call String function.
+
+
+// Array function  (For-loop)
+
+// For each monster, must run battle function.
+
+
+// String function  (String concatenation)
+
+// If fed Happy ending. + Grok + happy words.
+// If fed Sad ending. + Grok + sad words.
