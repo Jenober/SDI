80648f5f8c36105e59309b88c46dc3f159748673
Houston Bennett <Jenober@fullsail.edu>
2012-11-29 19:13:28 -0800

    Adding in final calls. String function and endings.

--- Project2/Bennett_Houston_assignment2.js
+++ Project2/Bennett_Houston_assignment2.js
@@ -7,52 +7,77 @@
 // Player Variables
 var playerName = "Grok",
     playerHP = 100,
-    playerStr = 15
+    playerStr = 15,
+    arenaDays = 0
 ;
 // Monster Variables
-var mobName = ["Giant Rat", "Gnoll","Dire Bear"],
-    mobHP = [5,15,50],
-    mobStr = [1,10,15]
+var mobName = ["Giant Rat", "Gnoll","Dire Bear","Dragon"],
+    mobHP = [5,15,50,200],
+    mobStr = [1,10,15,20]
     mobCount = mobName.length;
 ;
+// String function  (String concatenation)
+var getEnding = function (ending) {
+    if ending = "sad"{
+        console.log()
 
-// Procedure
-var preBattle = function (equipped, rested) {
-
-    if ((equipped = true) && (rested = true)) {
-        playerHP == 100;
-        console.log("You are rested and equipped for combat " + playerName + ".");
-        console.log("Steel your nerves for you are about to enter the Arena!");
-    }
-    else {
-        console.log("You are too tired and ill-equipped to fight right now, "+ playerName + ".")
     }
 
 
 };
+// If fed Happy ending. + Grok + happy words.
+// If fed Sad ending. + Grok + sad words.
 
+// Procedure
+
+var preBattle = function (equipped, rested) {
+
+    if (mobCount > 0) {
+        playerHP == 100;
+        arenaDays ++;
+        console.log("Another day, another victory. You have been here for: "+ arenaDays + " days.")
+        console.log("You are rested and equipped for combat " + playerName + ".");
+        console.log("Steel your nerves for you are about to enter the Arena!");
+        console.log("");
+    }
+    else {
+        console.log("You have defeated all the monsters in the Arena.");
+        //stringFunc(happy)
+    }
+
+};
+
+preBattle(true,true);
 
 // Number function (While-loop)
 
 // battle code. While Mob's HP > 0 kill kill kill
 // Then goto boolean function for next battle or end.
-
 var battleFunc = function (mobName, mobHP, mobStr, playerName, playerHP, playerStr) {
 
     while (mobHP > 1){
 
         console.log(playerName + " hits " + mobName + " for " + playerStr + " damage!");
         mobHP-=playerStr;
+
         if (mobHP > 1){
-        console.log(mobName + " has only " + mobHP + " left!");
-        console.log(mobName + " hits " + playerName + " for " + mobStr + " damage!");
-        playerHP -= mobStr;
-        console.log(playerName + " has only " + playerHP + " left!");
+            console.log(mobName + " has only " + mobHP + "HP left!");
+            console.log(mobName + " hits " + playerName + " for " + mobStr + " damage!");
+            playerHP -= mobStr;
+            if (playerHP > 1){
+                console.log(playerName + " has only " + playerHP + "HP left!");
+                console.log("Will " + playerName + " be able to handle much more?!");
+                console.log("");
+            }
+            else{
+                isItOver(true,mobCount);
+            }
         }
         else {
-            console.log(mobName + " has " + mobHP + "HP!");
-            console.log(playerName + " has defeated the " + mobName + "!")
-            //CALL BOOLEAN. FEED GROK NOT DEAD(FALSE), mobCount -1
+
+            console.log(playerName + " has defeated the " + mobName + "!");
+
+            isItOver(false,mobCount);
         };
 
     };
@@ -63,7 +88,21 @@ var battleFunc = function (mobName, mobHP, mobStr, playerName, playerHP, playerS
 
 
 // Boolean function (Comparison)
+var isItOver = function (grokDead,) {
 
+    if ((grokDead == false) && (mobCount > 0))  {
+          mobCount--
+          preBattle(true,true);
+
+    }
+    else {
+           console.log("You fought bravely, " + playerName +", but the arena was too much for you.");
+           console.log("You have died, leaving " + mobCount + "monsters for the next guy.");
+           stringFunc(sad);
+
+    };
+
+};
 // Is Grok dead? Are the monsters all dead?
 // If Grok is dead. Sad ending. Call String function.
 // if Grok !dead next monster.
@@ -73,44 +112,20 @@ var battleFunc = function (mobName, mobHP, mobStr, playerName, playerHP, playerS
 // Array function  (For-loop)
 
 // For each monster, must run battle function.
+var nextMob = function () {
+    for (var i = 0, j = mobCount; i < j; i++ ) {
 
-battleFunc(mobName[],mobHP[],mobStr[],playerName,playerHP,playerStr);
-
-// String function  (String concatenation)
-
-// If fed Happy ending. + Grok + happy words.
-// If fed Sad ending. + Grok + sad words.
-
-//Procedure
+           battleFunc(mobName[i],mobHP[i],mobStr[i],playerName,playerHP,playerStr);
+    };
 
 
 
-// Boolean Function (Comparison)
+};
 
-//Check if player is still alive.
-// Yes, check if more monsters.
-//      Yes, next battle, call array function.
-// No more mobs, call string function with happy ending.
-// If player is dead, call string function with sad ending.
+nextMob();
 
 
 
-// number Function (While-loop)
-
-// Battle code. Fed each monster. While time remains, mob and player attack each other.
-// After each round, call boolean function.
-
-
-
-// String Function (Concatenation)
-// If happy ending, playerName +  "Happy ending"
-//  else, playerName + "Sad ending"
-
-
-// Array Function (For-loop)
-
-// For each monster in the array, we call the number function to run
-// through the battle code.
 
 
 
