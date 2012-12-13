7e0075769c3a1252401d17f80b18880376a0c458
Houston Bennett <Jenober@fullsail.edu>
2012-12-13 10:11:46 -0800

    Starting phone number pattern checker.

--- Project4/rename-me.js
+++ Project4/rename-me.js
@@ -4,12 +4,32 @@
  as well as another string separator, return a string with the first separator
  changed to the second: "a,b,c" + "," + "/" → "a/b/c".
  */
-var str = "1 & 2 & 3 & 4";
-alert ("The string we are starting with is: " + str);
-var separator = prompt("Please enter the separator you're looking for: ");
-var newseparator = prompt("Please enter the separator that you'd like to replace your previous entry with: ");
+var swapSeparator = function(string){
+    var str = string;
+    alert ("The string we are starting with is: " + str);
+    var separator = prompt("Please enter the separator you're looking for: ");
+    var newseparator = prompt("Please enter the separator that you'd like to replace your previous entry with: ");
 
 
-console.log("You started with: " + str);
-var newstr = str.replace(separator, newseparator, "gi");
-console.log("Now you have: " + newstr);
+    console.log("You started with: " + str);
+    var newstr = str.replace(separator, newseparator, "gi");
+    console.log("Now you have: " + newstr);
+
+};
+
+/*
+ Does a string follow a 123-456-7890 pattern like a phone number?
+ */
+var isPhoneNum = function(phonenumber){
+    var phoneNum = phonenumber;
+
+    if(phoneNum.charAt(3) == "-" && phoneNum.charAt(7)== "-"){
+        alert("You have entered the phone number properly!")
+            
+    }else{
+
+        alert("You have entered the phone number improperly! Try again!")
+    }
+
+
+};
