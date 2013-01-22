74173bc722c8a3648d59cb60459d7255e33b6ffb
Houston Bennett <Jenober@fullsail.edu>
2013-01-22 12:33:04 -0800

    More Project 2, dynamic dropdown

--- VFW/Project 2/Bennett_Houston_Project2/js/main.js
+++ VFW/Project 2/Bennett_Houston_Project2/js/main.js
@@ -1,7 +1,73 @@
 /**
- * Created with JetBrains WebStorm.
- * User: Houston
+
+ * Name: Houston Bennett
  * Date: 1/9/13
- * Time: 3:39 PM
- * To change this template use File | Settings | File Templates.
+ * Term: 0113
+ * Project 2 Javascript
+
  */
+
+
+
+window.addEventListener("DOMContentLoaded",function(){
+
+/** All JS must be in here. */
+
+    //getElementbyID function
+    function getElement(elementID){
+
+    var x = document.getElementById(elementID);
+    return x;
+}
+
+    //Bring forth the dropdown box and populate it fruitfully!
+
+    function createDropdown(){
+        var formTag = document.getElementsByTagName("form"),
+            selectLI = getElement("dropdown"),
+            createSelect = document.createElement("select");
+        createSelect.setAttribute("id", "charSize");
+
+        for( var i = 0, x = charSizeOpt.length; i < x; i++ ){
+            var Option = document.createElement("option");
+            var OptTxt = charSizeOpt[i];
+            Option.setAttribute("value",OptTxt);
+            Option.innerHTML = OptTxt;
+            createSelect.appendChild(Option);
+        };
+        selectLI.appendChild(createSelect);
+
+
+    }
+
+//dropdown defaults
+
+    var charSizeOpt = ["Select a size","Tiny","Small","Medium","Large","Giant"]
+    createDropdown();
+//Click events
+
+    var viewLink = getElement("ViewAll");
+    var submitBtn = getElement("SubmitBtn")
+    var deleteLink = getElement("DeleteAll");
+
+
+
+    viewLink.addEventListener("click", getData);
+    deleteLink.addEventListener("click", deleteData);
+    submitBtn.addEventListener("click",SUBMIT);
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
