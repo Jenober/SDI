757b8b08b82a2eb06a39a4f312de39a15ec0677a
Houston Bennett <Jenober@fullsail.edu>
2013-01-22 14:35:30 -0800

    Radio buttons and local storage work

--- VFW/Project 2/Bennett_Houston_Project2/js/main.js
+++ VFW/Project 2/Bennett_Houston_Project2/js/main.js
@@ -39,22 +39,69 @@ window.addEventListener("DOMContentLoaded",function(){
 
 
     }
+//Find radio button value
+    function getRadio(){
+        var rdoButtons = document.forms[0].sex;
+        for (var i=0; i < rdoButtons.length; i++){
+            if(rdoButtons[i].checked){
+                genderVal = rdoButtons[i].value;
+            }
+
+
+        }
+
+    }
+
+
+    function saveData(){
+
+        var userID = Math.floor(Math.random()*100001);
+        //Get radio button value
+        getRadio();
+
+        //Get all form field values & store them in an object.
+        //Object properties contain array with form label and input values.
+
+        var formFieldValues = {};
+
+            formFieldValues.Size = ["Size: ",getElement("charSize").value];
+            formFieldValues.Name = ["Name: ",getElement('charName').value];
+            formFieldValues.Race = ["Race: ",getElement('charRace').value];
+            formFieldValues.Age = ["Age: ",getElement('charAge').value];
+
+            formFieldValues.Gender = ["Gender: ", genderVal];
+
+            formFieldValues.Str = ["Strength: ",getElement("charStr").value];
+            formFieldValues.Con = ["Constitution: ",getElement("charCon").value];
+            formFieldValues.Int = ["Intelligence: ",getElement("charInt").value];
+            formFieldValues.Wis = ["Wisdom: ",getElement("charWis").value];
+            formFieldValues.Cha = ["Charisma: ",getElement('charCha').value];
+            formFieldValues.Bio = ["Biography: ",getElement('charBio').value];
+
+        //Save data to local storage: Use Stringify to convert object to string
+
+        localStorage.setItem(userID,JSON.stringify(formFieldValues));
+        alert("This is your character's unique ID. Save this for later! "+ userID)
+        alert("Your character has been archived!");
+    }
 
 //dropdown defaults
 
-    var charSizeOpt = ["Select a size","Tiny","Small","Medium","Large","Giant"]
+    var charSizeOpt = ["Select a size","Tiny","Small","Medium","Large","Giant"],genderVal
+        ;
     createDropdown();
 //Click events
 
     var viewLink = getElement("ViewAll");
-    var submitBtn = getElement("SubmitBtn")
+    var submitBtn = getElement("SubmitBtn");
+    submitBtn.addEventListener("click",saveData);
     var deleteLink = getElement("DeleteAll");
 
 
 
     viewLink.addEventListener("click", getData);
     deleteLink.addEventListener("click", deleteData);
-    submitBtn.addEventListener("click",SUBMIT);
+
 
 
 
